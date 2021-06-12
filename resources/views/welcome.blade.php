631740d5acc3d3007855b1178c1d20d8aca2611a
putrinovriantiiswanto <putri.novriantiis.22@gmail.com>
2021-06-12 19:31:32 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app_0304')
+
+@section('title', 'Home')
+
+@section('content')
+<marquee scrollamount="10">TEKNIK INFORMATIKA - UNIVERSITAS 17 AGUSTUS 1945 SURABAYA</marquee>
+<h1>Putri Novrianti Iswanto</h1>
+<h1>1461900304</h1>
+    
+@endsection
